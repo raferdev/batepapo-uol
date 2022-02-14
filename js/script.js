216bb91e83d4d2b0f1fe09df9df107cc7879193f
Rafael <rafinha_fern@hotmail.com>
2022-02-14 04:41:37 -0300

    v2.0
    post nome, verificacao de online

--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
+let nome = ""
+let nomeOnline = {}
+function processarResposta() {
+    let nomeOnline = document.querySelector(".chat")
+    
+    let nav = document.querySelector(".entrada")
+    nav.classList.toggle('visivel')
+    setInterval(chatOnline,5000)
+}
+function chatOnline () {
+    axios.post("https://mock-api.driven.com.br/api/v4/uol/status", nomeOnline);
+    console.log('bunts')
+}
+function errorNome () {
+    alert('tente outro nome')
+    console.log('bunts')
+}
 function navVisivel () {
     let nav = document.querySelector(".nav")
     nav.classList.toggle('visivel')
+}
+function entradaVisivel () {
+    nome = document.querySelector(".nameInput").value
+    nomeOnline = {
+    name: nome };
+    const requisicaoNome = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", nomeOnline);
+    requisicaoNome.then(processarResposta);
+    requisicaoNome.catch(errorNome)
 }
