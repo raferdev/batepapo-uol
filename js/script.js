53c7de8465163c18fbddbc3360c52e03301e4160
Rafael <rafinha_fern@hotmail.com>
2022-02-14 05:55:36 -0300

    v 1.0
    mais coisas

--- js/script.js
+++ js/script.js
@@ -1,19 +1,37 @@
 let nome = ""
 let nomeOnline = {}
+let chatMsgs = {}
+let pParticipantes = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages")
+    pParticipantes.then(carregarParticipantes)
+
 function processarResposta() {
-    let nomeOnline = document.querySelector(".chat")
-    
-    let nav = document.querySelector(".entrada")
-    nav.classList.toggle('visivel')
+    let entrada = document.querySelector(".entrada")
+    const promisseChatMsgs = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages")
+    entrada.classList.toggle('visivel')
     setInterval(chatOnline,5000)
+    aplicarStatus()
+}
+function carregarParticipantes(participantes) {
+    let listaParticipantes = document.querySelector(".sidebarOnlineUserConainer");
+    console.log(participantes.length)
+    for (let i = 0; i < participantes.length; i++) {
+        console.log("1")
+        listaParticipantes.innerHTML += `<div class="sidebarOnlineUser"><div class="sidebarLines"><ion-icon name="person-circle"></ion-icon><p class="sidebarTxt">${participantes[i].name}</p></div><ion-icon class="iconCheck" name="checkmark"></ion-icon></div>`
+    }
+}
+
+function charregarChat(promisseChatmsgs) {
+    chatMsgs = promisseChatMsgs
+}
+function aplicarStatus() {
+    let status = document.querySelector(".chat")
+    status.innerHTML += `<div class="msg chatStatus"><div class = "hora">testehora</div><div class = "nome">${nome}</div><div class="textoMsg">entrou na sala...</div></div>`
 }
 function chatOnline () {
     axios.post("https://mock-api.driven.com.br/api/v4/uol/status", nomeOnline);
-    console.log('bunts')
 }
 function errorNome () {
     alert('tente outro nome')
-    console.log('bunts')
 }
 function navVisivel () {
     let nav = document.querySelector(".nav")
