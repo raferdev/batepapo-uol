465bc897c5e4f7b97c6b9ebe4a1aae5be3f0030d
Rafael <rafinha_fern@hotmail.com>
2022-10-10 23:37:16 -0300

    fix: new initial photo

--- js/script.js
+++ js/script.js
@@ -17,15 +17,18 @@ function processarResposta() {
     setInterval(carregarChat,5000)
     aplicarStatus()
 }
+
 function carregarParticipantes() {
     let pParticipantes = axios.get("https://mock-api.driven.com.br/api/v4/uol/participants")
     pParticipantes.then(funcaoParticipantes)
 }
+
 function funcaoParticipantes (pParticipantes) {
     participantes = pParticipantes.data
     listaParticipantes.innerHTML = ""
     participantes.forEach(funcaoAddParticipantes)
 }
+
 function funcaoAddParticipantes (participante) {
         listaParticipantes.innerHTML += `<div class="sidebarOnlineUser ${participante.name}" onclick = "selectionUser('${participante.name}')"><div class="sidebarLines"><ion-icon name="person-circle"></ion-icon><p class="sidebarTxt">${participante.name}</p></div><ion-icon class="iconCheck" name="checkmark"></ion-icon></div>`
     }
@@ -35,33 +38,41 @@ function selectionUser(destinatario) {
     let elemento = document.querySelector("."+ para + " .iconCheck")
     elemento.classList.add("check")
 }
+
 function carregarChat() {
     let promisseChatMsgs =  axios.get("https://mock-api.driven.com.br/api/v4/uol/messages")
     promisseChatMsgs.then(printarChat)
 }
+
 function printarChat(pChatOnline) {
     chatOnlineNovo = pChatOnline.data
     listaChat.innerHTML = ""
     chatOnlineNovo.forEach(addChat)
 }
+
 function addChat(msgsChat){
     listaChat.innerHTML += `<div data-identifier="message" class="msg"><div class = "hora">${msgsChat.time}</div><div class = "nome">${msgsChat.from}</div><div class = "para">para </div><div class = "quem">${msgsChat.to}:</div><div class="textoMsg">${msgsChat.text}</div></div>`   
 }
+
 function aplicarStatus() {
     let status = document.querySelector(".chat")
     status.innerHTML += `<div class="msg chatStatus"><div class = "hora">testehora</div><div class = "nome">${nome}</div><div class="textoMsg">entrou na sala...</div></div>`
     status.scrollIntoView()
 }
+
 function chatOnline () {
     axios.post("https://mock-api.driven.com.br/api/v4/uol/status", nomeOnline);
 }
+
 function errorNome () {
     alert('tente outro nome')
 }
+
 function navVisivel () {
     let nav = document.querySelector(".nav")
     nav.classList.toggle('visivel')
 }
+
 function entradaVisivel () {
     nome = document.querySelector(".nameInput").value
     nomeOnline = {
@@ -70,6 +81,7 @@ function entradaVisivel () {
     requisicaoNome.then(processarResposta);
     requisicaoNome.catch(errorNome)
 }
+
 function enviarMsg() {
     let msgTxT = document.querySelector(".msgInput").value
     let msg = {
